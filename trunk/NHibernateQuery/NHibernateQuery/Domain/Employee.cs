de698adb25b8b64201ea4244587ba459d9c97066
igor.israel <igor.israel@d77c11c2-dd88-11de-8752-9f8e91036772>
2010-02-08 14:21:53 +0000

    add NHibernateQuery project

---
+++ trunk/NHibernateQuery/NHibernateQuery/Domain/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernateQuery.Domain
+{
+    class Employee
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual double Salary { get; set; }
+        public virtual int Age { get; set; }
+    }
+}
